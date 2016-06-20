4e6286c0a7836c01e25dd1dfcfd7fd31604a33a6
annechen <annechen@fb.com>
2016-06-20 11:33:17 -0700

    new commit - Can I Drink app

---
+++ app/src/main/java/com/codepath/canidrink/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.codepath.canidrink;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public EditText etAge;
+    //public int age;
+    public User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        etAge = (EditText) findViewById(R.id.etAge);
+
+        user = (User)getIntent().getSerializableExtra("user");
+
+        String ageText = String.valueOf(user.age);
+        etAge.setText(ageText);
+
+    }
+
+    public void onSubmit(View view) {
+
+        String ageText = etAge.getText().toString();
+        int age = Integer.parseInt(ageText);
+
+        Intent intent = new Intent();
+        user.age = age;
+
+        intent.putExtra("user", user);
+        intent.putExtra("age", age);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+}
---
+++ app/src/main/java/com/codepath/canidrink/User.java
@@ -0,0 +1,12 @@
+package com.codepath.canidrink;
+
+import java.io.Serializable;
+
+/**
+ * Created by annechen on 6/20/16.
+ */
+public class User implements Serializable {
+    public String name;
+    public int age;
+    public String birthday;
+}
